654bebe56cc0212e999f40ecbc857c0abe6e1994
Aimee Liang <aimeeeliang@gmail.com>
2020-08-23 16:16:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,9 +5,9 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  cart.each do |item, nested_hash|
-  if nested_hash[:clearance] == true
-    nested_hash[:price] = (nested_hash[:price]*0.8).round(2)
+  cart.each do |item|
+  if item[:clearance] == true
+    item[:price] = (item[:price]*0.8).round(2)
   end
   end
   cart
